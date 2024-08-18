078c4d95ce8b8692316ab8fd1c79d1ada569ccb1
Luong Van Linh <luongvanlinh12@gmail.com>
2024-08-18 09:42:07 +0700

    init project

--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -1,5 +1,7 @@
 package com.example.demo.model.persistence;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,52 +12,48 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 
 @Entity
 @Table(name = "user")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@JsonProperty
-	private long id;
-	
-	@Column(nullable = false, unique = true)
-	@JsonProperty
-	private String username;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "cart_id", referencedColumnName = "id")
-	@JsonIgnore
-    private Cart cart;
-	
-	public Cart getCart() {
-		return cart;
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonProperty
+  private long id;
 
-	public void setCart(Cart cart) {
-		this.cart = cart;
-	}
+  @Column(nullable = false, unique = true)
+  @JsonProperty
+  private String username;
 
-	public long getId() {
-		return id;
-	}
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "cart_id", referencedColumnName = "id")
+  @JsonIgnore
+  private Cart cart;
 
-	public void setId(long id) {
-		this.id = id;
-	}
+  public Cart getCart() {
+    return cart;
+  }
 
-	public String getUsername() {
-		return username;
-	}
+  public void setCart(Cart cart) {
+    this.cart = cart;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	
-	
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+
 }
